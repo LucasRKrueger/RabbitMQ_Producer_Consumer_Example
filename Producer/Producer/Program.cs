e55b785237d79288c00999dac7eca82e23354d79
LucasRKrueger <lucasrkthd@gmail.com>
2020-06-27 13:30:46 -0300

    Project

---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "qwebapp",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                int count = 0;
+                while (count < 1000)
+                {
+                    string message = $"Count: {count}, Msg: Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "qwebapp",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine($"[x] Sent {message}");
+                    count++;
+                }
+            }
+        }
+    }
+}
